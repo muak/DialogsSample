c8bf4947490e16b04f9d2a7d639ccc43277a0219
Satoshi Nakamura <kamu@kamusoft-mini-2.local>
2020-05-28 20:34:01 +0900

    First commit

---
+++ DialogsSample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DialogsSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if(await AiForms.Dialogs.Dialog.Instance.ShowAsync<SampleDialog>(new { Message = "Hello" }))
+            {
+                System.Diagnostics.Debug.WriteLine("OK Tapped");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Cancel Tapped");
+            }
+        }
+    }
+}
---
+++ DialogsSample/SampleDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using AiForms.Dialogs.Abstractions;
+using Xamarin.Forms;
+
+namespace DialogsSample
+{
+    public partial class SampleDialog : DialogView
+    {
+        public SampleDialog()
+        {
+            InitializeComponent();
+        }
+
+        void OK_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DialogNotifier.Complete();
+        }
+
+        void Cancel_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DialogNotifier.Cancel();
+        }
+    }
+}
